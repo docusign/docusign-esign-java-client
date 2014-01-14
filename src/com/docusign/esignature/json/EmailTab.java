11b80595ae4e0bd3e1c3ec498b151719e2aa32a4
Mike Borozdin <mikeb@docusign.com>
2014-01-14 14:31:55 -0800

    re-added the copyrights

--- src/com/docusign/esignature/json/EmailTab.java
+++ src/com/docusign/esignature/json/EmailTab.java
@@ -1,4 +1,19 @@
-
+/**
+ * @copyright Copyright (C) DocuSign, Inc.  All rights reserved.
+ *
+ * This source code is intended only as a supplement to DocuSign SDK
+ * and/or on-line documentation.
+ * 
+ * This sample is designed to demonstrate DocuSign features and is not intended
+ * for production use. Code and policy for a production application must be
+ * developed to meet the specific data and security requirements of the
+ * application.
+ *
+ * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY
+ * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
+ * PARTICULAR PURPOSE.
+ */
 package com.docusign.esignature.json;
 
 import java.util.HashMap;
