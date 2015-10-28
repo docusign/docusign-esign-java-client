6d3cef73d4213c704c784e77e1ffcab20a8ac206
Mike Borozdin <nolongemails@gmail.com>
2015-10-28 07:44:18 -0700

    fixed a typoe updated .gitignore updated build properties included test resources in pom.xml

--- tests/com/docusign/esignature/tests/TestSettings.java
+++ tests/com/docusign/esignature/tests/TestSettings.java
@@ -6,13 +6,13 @@ public class TestSettings {
 	// www.docusign.com/devcenter , set up an account
 	// and then go to preferences -> API to get your 
 	// integrator key
-	public static final String TEST_EMAIL = "TODO";
-	public static final String TEST_PASSWORD = "TODO";
-	public static final String TEST_INTEGRATOR_KEY = "TODO - GUID";
+	public static final String TEST_EMAIL = "nolongemails@gmail.com";
+	public static final String TEST_PASSWORD = "RunLevel1";
+	public static final String TEST_INTEGRATOR_KEY = "DOCU-e04c92f9-0df4-4dae-8d05-f16575128f0b";
 	// this is a template with just one signer "Signer1"
-	public static final String TEMPLATE_ID = "TODO - GUID";
+	public static final String TEMPLATE_ID = "847a7544-6ac2-4058-91c5-0075d76994a0";
 	//
 	// here use an e-mail that will get all your test signature requests
-	public static final String SAMPLE_EMAIL = "TODO";
-	public static final String SENT_ENVELOPE = "TODO - GUID";
+	public static final String SAMPLE_EMAIL = "mikebz@outlook.com";
+	public static final String SENT_ENVELOPE = "1df99f31-c313-40cd-a784-92d114ab4de7";
 }
