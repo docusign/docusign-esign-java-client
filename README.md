4b04806a6dad64cdcf65738fa064bdbd686f19da
Majid Mallis <majid.mallis@docusign.com>
2018-05-16 00:11:05 -0700

    version 2.6.0
    See CHANGELOG.md

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ You can sign up for a free [developer sandbox](https://developers.docusign.com/)
 Requirements
 ============
 
-Java 1.6 or later.  
+Java 1.7 or later.  
 
 Installation
 ============
@@ -21,7 +21,7 @@ Add this dependency to your project's POM:
 <dependency>
    <groupId>com.docusign</groupId>
    <artifactId>docusign-esign-java</artifactId>
-   <version>2.5.1</version>
+   <version>2.6.0</version>
 </dependency>
 ```
 
@@ -30,7 +30,7 @@ Add this dependency to your project's POM:
 Add this dependency to your project's build file:
 
 ```groovy
-compile "com.docusign:docusign-esign-java:2.5.1"
+compile "com.docusign:docusign-esign-java:2.6.0"
 ```
 
 #### Dependencies
@@ -73,8 +73,8 @@ android {
 
 This client is available through the following Java package managers:
 
-- [Nexus Repository Manager](https://oss.sonatype.org/#nexus-search;quick~docusign-esign-java) (oss.sonatype.org). You can search for com.docusign or docusign-esign-java. The current version is 2.5.1.
-- [JFrog Bintray](https://bintray.com/dsdevcenter/maven/docusign-esign-java) (bintray.com). You can search for com.docusign or docusign-esign-java. The current version is 2.5.1.
+- [Nexus Repository Manager](https://oss.sonatype.org/#nexus-search;quick~docusign-esign-java) (oss.sonatype.org). You can search for com.docusign or docusign-esign-java. The current version is 2.6.0.
+- [JFrog Bintray](https://bintray.com/dsdevcenter/maven/docusign-esign-java) (bintray.com). You can search for com.docusign or docusign-esign-java. The current version is 2.6.0.
 
 
 Usage
--- src/main/java/com/docusign/esign/client/auth/OAuth.java
+++ src/main/java/com/docusign/esign/client/auth/OAuth.java
@@ -2,26 +2,42 @@ package com.docusign.esign.client.auth;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import javax.ws.rs.core.Response;
 
-import org.apache.oltu.oauth2.client.HttpClient;
 import org.apache.oltu.oauth2.client.OAuthClient;
 import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
 import org.apache.oltu.oauth2.client.response.OAuthJSONAccessTokenResponse;
 import org.apache.oltu.oauth2.client.request.OAuthClientRequest.AuthenticationRequestBuilder;
 import org.apache.oltu.oauth2.client.request.OAuthClientRequest.TokenRequestBuilder;
 import org.apache.oltu.oauth2.common.message.types.GrantType;
-import org.apache.oltu.oauth2.common.message.types.ResponseType;
 import org.apache.oltu.oauth2.common.token.BasicOAuthToken;
 
 import com.docusign.esign.client.Pair;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.sun.jersey.api.client.Client;
 import com.sun.jersey.api.client.ClientHandlerException;
+
+import io.swagger.annotations.ApiModelProperty;
 
 @javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaClientCodegen", date = "2017-03-06T16:42:36.211-08:00")
 public class OAuth implements Authentication {
 	static final int MILLIS_PER_SECOND = 1000;
+
+	// OAuth Scope constants
+	/** create and send envelopes, and obtain links for starting signing sessions. */
+	public final static String Scope_SIGNATURE = "signature";
+	/** obtain a refresh token with an extended lifetime. */
+	public final static String Scope_EXTENDED = "extended";
+	/** obtain access to the user’s account when the user is not present. */
+	public final static String Scope_IMPERSONATION = "impersonation";
+
+	// OAuth ResponseType constants
+	/** used by public/native client applications. */
+	public final static String CODE = "code";
+	/** used by private/trusted client application. */
+	public final static String TOKEN = "token";
 
 	private volatile String accessToken;
 	private Long expirationTimeMillis;
@@ -42,11 +58,11 @@ public class OAuth implements Authentication {
 		switch (flow) {
 		case accessCode:
 			tokenRequestBuilder.setGrantType(GrantType.AUTHORIZATION_CODE);
-			authenticationRequestBuilder.setResponseType(ResponseType.CODE.name().toLowerCase());
+			authenticationRequestBuilder.setResponseType(OAuth.CODE);
 			break;
 		case implicit:
 			tokenRequestBuilder.setGrantType(GrantType.IMPLICIT);
-			authenticationRequestBuilder.setResponseType(ResponseType.TOKEN.name().toLowerCase());
+			authenticationRequestBuilder.setResponseType(OAuth.TOKEN);
 			break;
 		case password:
 			tokenRequestBuilder.setGrantType(GrantType.PASSWORD);
@@ -150,5 +166,512 @@ public class OAuth implements Authentication {
 
 	public void setOauthClient(Client client) {
 		this.oauthClient = new OAuthClient(new OAuthJerseyClient(client));
+	}
+	
+	/**
+	 * 
+	 * OAuthToken model with the following properties:
+	 * <br><b>accessToken</b>: the token you will use in the Authorization header of calls to the DocuSign API.
+	 * <br><b>tokenType</b>: this is the type of the accessToken. It is usually "Bearer".
+	 * <br><b>refreshToken</b>: a token you can use to get a new accessToken without requiring user interaction.
+	 * <br><b>expiresIn</b>: the number of seconds before the accessToken expires.
+	 *
+	 */
+	public static class OAuthToken {
+		@JsonProperty("access_token")
+		private String accessToken = null;
+
+		@JsonProperty("token_type")
+		private String tokenType = null;
+
+		@JsonProperty("refresh_token")
+		private String refreshToken = null;
+
+		@JsonProperty("expires_in")
+		private Long expiresIn = 0L;
+
+		public OAuthToken accessToken(String accessToken) {
+			this.accessToken = accessToken;
+			return this;
+		}
+
+		/**
+		 * Get accessToken
+		 *
+		 * @return accessToken
+		 **/
+		@ApiModelProperty(example = "null", value = "")
+		public String getAccessToken() {
+			return accessToken;
+		}
+
+		public void setAccessToken(String accessToken) {
+			this.accessToken = accessToken;
+		}
+
+		public OAuthToken isTokenType(String tokenType) {
+			this.tokenType = tokenType;
+			return this;
+		}
+
+		/**
+		 * Get tokenType
+		 *
+		 * @return tokenType
+		 **/
+		@ApiModelProperty(example = "null", value = "")
+		public String getTokenType() {
+			return tokenType;
+		}
+
+		public void setTokenType(String tokenType) {
+			this.tokenType = tokenType;
+		}
+
+		public OAuthToken refreshToken(String refreshToken) {
+			this.refreshToken = refreshToken;
+			return this;
+		}
+
+		/**
+		 * Get refreshToken
+		 * 
+		 * @return refreshToken
+		 **/
+		@ApiModelProperty(example = "null", value = "")
+		public String getRefreshToken() {
+			return refreshToken;
+		}
+
+		public void setRefreshToken(String refreshToken) {
+			this.refreshToken = refreshToken;
+		}
+
+		public OAuthToken expiresIn(Long expiresIn) {
+			this.expiresIn = expiresIn;
+			return this;
+		}
+
+		/**
+		 * Get expiresIn
+		 * 
+		 * @return expiresIn
+		 **/
+		@ApiModelProperty(example = "3600L", value = "0L")
+		public Long getExpiresIn() {
+			return expiresIn;
+		}
+
+		public void setExpiresIn(Long expiresIn) {
+			this.expiresIn = expiresIn;
+		}
+
+		@Override
+		public boolean equals(java.lang.Object o) {
+			if (this == o) {
+				return true;
+			}
+			if (o == null || getClass() != o.getClass()) {
+				return false;
+			}
+			OAuthToken account = (OAuthToken) o;
+			return Objects.equals(this.accessToken, account.accessToken)
+					&& Objects.equals(this.tokenType, account.tokenType)
+					&& Objects.equals(this.refreshToken, account.refreshToken)
+					&& Objects.equals(this.expiresIn, account.expiresIn);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(accessToken, tokenType, refreshToken, expiresIn);
+		}
+
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			sb.append("class OAuthToken {\n");
+
+			sb.append("    accessToken: ").append(toIndentedString(accessToken)).append("\n");
+			sb.append("    tokenType: ").append(toIndentedString(tokenType)).append("\n");
+			sb.append("    refreshToken: ").append(toIndentedString(refreshToken)).append("\n");
+			sb.append("    expiresIn: ").append(toIndentedString(expiresIn)).append("\n");
+			return sb.toString();
+		}
+
+		/**
+		 * Convert the given object to string with each line indented by 4
+		 * spaces (except the first line).
+		 */
+		private String toIndentedString(java.lang.Object o) {
+			if (o == null) {
+				return "null";
+			}
+			return o.toString().replace("\n", "\n    ");
+		}
+	}
+	
+	/**
+	 * 
+	 * UserInfo model with the following properties:
+	 * <br><b>sub</b>: the user ID GUID.
+	 * <br><b>accounts</b>: this is list of DocuSign accounts associated with the current user.
+	 * <br><b>name</b>: the user's full name.
+	 * <br><b>givenName</b>: the user's given name.
+	 * <br><b>familyName</b>: the user's family name.
+	 * <br><b>email</b>: the user's email address.
+	 * <br><b>created</b>: the UTC DateTime when the user login was created.
+	 *
+	 * @see Account
+	 *
+	 */
+	public static class UserInfo {
+		/**
+		 * 
+		 * Account model with the following properties:
+		 * <br><b>accountId</b>: the account ID GUID.
+		 * <br><b>isDefault</b>: whether this is the default account, when the user has access to multiple accounts.
+		 * <br><b>accountName</b>: the human-readable name of the account.
+		 * <br><b>baseUri</b>: the base URI associated with this account.
+		 * It also tells which DocuSign data center the account is hosted on.
+		 *
+		 */
+		public static class Account {
+			@JsonProperty("account_id")
+			private String accountId = null;
+
+			@JsonProperty("is_default")
+			private String isDefault = null;
+
+			@JsonProperty("account_name")
+			private String accountName = null;
+
+			@JsonProperty("base_uri")
+			private String baseUri = null;
+
+			public Account accountId(String accountId) {
+				this.accountId = accountId;
+				return this;
+			}
+
+			/**
+			 * Get accountId
+			 *
+			 * @return accountId
+			 **/
+			@ApiModelProperty(example = "null", value = "")
+			public String getAccountId() {
+				return accountId;
+			}
+
+			public void setAccountId(String accountId) {
+				this.accountId = accountId;
+			}
+
+			public Account isDefault(String isDefault) {
+				this.isDefault = isDefault;
+				return this;
+			}
+
+			/**
+			 * Get isDefault
+			 *
+			 * @return isDefault
+			 **/
+			@ApiModelProperty(example = "null", value = "")
+			public String getIsDefault() {
+				return isDefault;
+			}
+
+			public void setIsDefault(String isDefault) {
+				this.isDefault = isDefault;
+			}
+
+			public Account accountName(String accountName) {
+				this.accountName = accountName;
+				return this;
+			}
+
+			/**
+			 * Get accountName
+			 * 
+			 * @return accountName
+			 **/
+			@ApiModelProperty(example = "null", value = "")
+			public String getAccountName() {
+				return accountName;
+			}
+
+			public void setAccountName(String accountName) {
+				this.accountName = accountName;
+			}
+
+			public Account baseUri(String baseUri) {
+				this.baseUri = baseUri;
+				return this;
+			}
+
+			/**
+			 * Get baseUri
+			 * 
+			 * @return baseUri
+			 **/
+			@ApiModelProperty(example = "null", value = "")
+			public String getBaseUri() {
+				return baseUri;
+			}
+
+			public void setBaseUri(String baseUri) {
+				this.baseUri = baseUri;
+			}
+
+			@Override
+			public boolean equals(java.lang.Object o) {
+				if (this == o) {
+					return true;
+				}
+				if (o == null || getClass() != o.getClass()) {
+					return false;
+				}
+				Account account = (Account) o;
+				return Objects.equals(this.accountId, account.accountId)
+						&& Objects.equals(this.isDefault, account.isDefault)
+						&& Objects.equals(this.accountName, account.accountName)
+						&& Objects.equals(this.baseUri, account.baseUri);
+			}
+
+			@Override
+			public int hashCode() {
+				return Objects.hash(accountId, isDefault, accountName, baseUri);
+			}
+
+			@Override
+			public String toString() {
+				StringBuilder sb = new StringBuilder();
+				sb.append("class Account {\n");
+
+				sb.append("    accountId: ").append(toIndentedString(accountId)).append("\n");
+				sb.append("    isDefault: ").append(toIndentedString(isDefault)).append("\n");
+				sb.append("    accountName: ").append(toIndentedString(accountName)).append("\n");
+				sb.append("    baseUri: ").append(toIndentedString(baseUri)).append("\n");
+				return sb.toString();
+			}
+
+			/**
+			 * Convert the given object to string with each line indented by 4
+			 * spaces (except the first line).
+			 */
+			private String toIndentedString(java.lang.Object o) {
+				if (o == null) {
+					return "null";
+				}
+				return o.toString().replace("\n", "\n    ");
+			}
+		}
+
+		@JsonProperty("sub")
+		private String sub = null;
+
+		@JsonProperty("email")
+		private String email = null;
+
+		@JsonProperty("accounts")
+		private java.util.List<Account> accounts = new java.util.ArrayList<Account>();
+
+		@JsonProperty("name")
+		private String name = null;
+
+		@JsonProperty("given_name")
+		private String givenName = null;
+
+		@JsonProperty("family_name")
+		private String familyName = null;
+
+		@JsonProperty("created")
+		private String created = null;
+		
+		public UserInfo sub(String sub) {
+			this.sub = sub;
+			return this;
+		}
+
+		/**
+		 * Get sub
+		 *
+		 * @return sub
+		 **/
+		@ApiModelProperty(example = "null", value = "")
+		public String getSub() {
+			return sub;
+		}
+
+		public void setSub(String sub) {
+			this.sub = sub;
+		}
+
+		public UserInfo email(String email) {
+			this.email = email;
+			return this;
+		}
+
+		/**
+		 * Get email
+		 *
+		 * @return email
+		 **/
+		@ApiModelProperty(example = "null", value = "")
+		public String getEmail() {
+			return email;
+		}
+
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		public UserInfo accounts(java.util.List<Account> accounts) {
+			this.accounts = accounts;
+			return this;
+		}
+
+		public UserInfo addAccountsItem(Account accountsItem) {
+			this.accounts.add(accountsItem);
+			return this;
+		}
+
+		/**
+		 * Get accounts
+		 * 
+		 * @return accounts
+		 **/
+		@ApiModelProperty(example = "null", value = "")
+		public java.util.List<Account> getAccounts() {
+			return accounts;
+		}
+
+		public void setAccounts(java.util.List<Account> accounts) {
+			this.accounts = accounts;
+		}
+
+		public UserInfo name(String name) {
+			this.name = name;
+			return this;
+		}
+
+		/**
+		 * Get name
+		 *
+		 * @return name
+		 **/
+		@ApiModelProperty(example = "null", value = "")
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public UserInfo givenName(String givenName) {
+			this.givenName = givenName;
+			return this;
+		}
+
+		/**
+		 * Get givenName
+		 *
+		 * @return givenName
+		 **/
+		@ApiModelProperty(example = "null", value = "")
+		public String getGivenName() {
+			return givenName;
+		}
+
+		public void setGivenName(String givenName) {
+			this.givenName = givenName;
+		}
+
+		public UserInfo familyName(String familyName) {
+			this.familyName = familyName;
+			return this;
+		}
+
+		/**
+		 * Get familyName
+		 *
+		 * @return familyName
+		 **/
+		@ApiModelProperty(example = "null", value = "")
+		public String getFamilyName() {
+			return familyName;
+		}
+
+		public void setFamilyName(String familyName) {
+			this.familyName = familyName;
+		}
+
+		public UserInfo created(String created) {
+			this.created = created;
+			return this;
+		}
+
+		/**
+		 * Get created
+		 *
+		 * @return created
+		 **/
+		@ApiModelProperty(example = "null", value = "")
+		public String getCreated() {
+			return created;
+		}
+
+		public void setCreated(String created) {
+			this.created = created;
+		}
+
+		@Override
+		public boolean equals(java.lang.Object o) {
+			if (this == o) {
+				return true;
+			}
+			if (o == null || getClass() != o.getClass()) {
+				return false;
+			}
+			UserInfo userInfo = (UserInfo) o;
+			return Objects.equals(this.sub, userInfo.sub) && Objects.equals(this.email, userInfo.email)
+					&& Objects.equals(this.accounts, userInfo.accounts) && Objects.equals(this.name, userInfo.name)
+					&& Objects.equals(this.givenName, userInfo.givenName) && Objects.equals(this.familyName, userInfo.familyName)
+					&& Objects.equals(this.created, userInfo.created);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(sub, email, accounts, name, givenName, familyName, created);
+		}
+
+		@Override
+		public String toString() {
+			StringBuilder sb = new StringBuilder();
+			sb.append("class UserInfo {\n");
+
+			sb.append("    sub: ").append(toIndentedString(sub)).append("\n");
+			sb.append("    email: ").append(toIndentedString(email)).append("\n");
+			sb.append("    name: ").append(toIndentedString(name)).append("\n");
+			sb.append("    givenName: ").append(toIndentedString(givenName)).append("\n");
+			sb.append("    familyName: ").append(toIndentedString(familyName)).append("\n");
+			sb.append("    created: ").append(toIndentedString(created)).append("\n");
+			sb.append("    accounts: ").append(toIndentedString(accounts)).append("\n");
+			return sb.toString();
+		}
+
+		/**
+		 * Convert the given object to string with each line indented by 4 spaces
+		 * (except the first line).
+		 */
+		private String toIndentedString(java.lang.Object o) {
+			if (o == null) {
+				return "null";
+			}
+			return o.toString().replace("\n", "\n    ");
+		}
+
 	}
 }
