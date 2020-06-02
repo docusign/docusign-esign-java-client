130c9cca89b3467e7c2bf9d76f612e57511b9500
Majid Mallis <mmallis87@gmail.com>
2020-06-02 11:43:41 -0700

    Version 3.6.0 release (#142)
    ### Changed
    - Added support for version v2.1-20.1.02 of the DocuSign eSignature API.
    - Updated the SDK release version.

--- src/main/java/com/docusign/esign/client/ApiClient.java
+++ src/main/java/com/docusign/esign/client/ApiClient.java
@@ -122,7 +122,7 @@ public class ApiClient {
     dateFormat = ApiClient.buildDefaultDateFormat();
 
     // Set default User-Agent.
-    setUserAgent("Swagger-Codegen/3.6.0-RC1/java");
+    setUserAgent("Swagger-Codegen/3.6.0/java");
 
     // Setup authentications (key: authentication name, value: authentication).
     authentications = new HashMap<String, Authentication>();
